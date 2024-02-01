18d2bfd10167b49647a04b7ebdce2d7391695634
Therese Weidenstedt <kw222mi@student.lnu.se>
2024-02-01 10:33:56 +0100

    add function for user guess

--- exercise-javascript-hangman-main/index.js
+++ exercise-javascript-hangman-main/index.js
@@ -1,30 +1,33 @@
+let theWordToGuess = [];
+let userGuess = [];
+let gameOver = false;
 
-let theWordToGuess = []
-let userGuess = []
-let gameOver = false
+let guess = document.getElementById("guess-form");
+guess.addEventListener("submit", (e) => {
+  handleUserGuess(e);
+});
 
-async function newWordToGuess () {
-    let word = await getRandomWord();;
-   console.log(word)
+async function newWordToGuess() {
+  let word = await getRandomWord();
+  console.log(word);
 
-    theWordToGuess = word.split(""); 
-    userGuess = Array.from(word, () => "_");
-    console.log(userGuess)
-    let numberOfLetters = word.length
-    console.log(numberOfLetters)
+  theWordToGuess = word.split("");
+  userGuess = Array.from(word, () => "_");
+  console.log(userGuess);
+  let numberOfLetters = word.length;
+  console.log(numberOfLetters);
 
-    renderEmptyWord(numberOfLetters);
-    startGame()
+  renderEmptyWord(numberOfLetters);
 }
 
- async function getRandomWord() {
-   const response = await fetch(
-     "https://random-word-api.vercel.app/api?words=1"
-   );
-   const movies = await response.json();
-   console.log( movies[0]);
-   return movies[0]
- }
+async function getRandomWord() {
+  const response = await fetch(
+    "https://random-word-api.vercel.app/api?words=1"
+  );
+  const movies = await response.json();
+  console.log(movies[0]);
+  return movies[0];
+}
 
 function renderEmptyWord(numberOfLetters) {
   let wordPlace = document.querySelector("#the-word-to-guess");
@@ -35,10 +38,20 @@ function renderEmptyWord(numberOfLetters) {
   wordPlace.appendChild(element);
 }
 
-function startGame () {
-    console.log('started')
+function handleUserGuess(e) {
+  e.preventDefault();
+  // Get the input inside the form
+  let guessInput = guess.querySelector("#guess");
+  // Get the value
+  let guessValue = guessInput.value;
+  console.log("Guess:", guessValue);
 
-    // while word to guess != user guess && game over = false, let user guess
+  checkTheGuess(guessValue);
+}
+
+function checkTheGuess(guessValue) {
+  let check = theWordToGuess.includes(guessValue);
+  console.log(check);
 }
 
 newWordToGuess();
